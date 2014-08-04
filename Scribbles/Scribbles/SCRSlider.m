c4a1a2b59abb70b148552d53d7410b5595c31d61
Shane Sniteman <ssniteman@gmail.com>
2014-08-04 15:46:48 -0400

    created SCRSlider and sliderView

--- Scribbles/Scribbles/SCRSlider.m
+++ Scribbles/Scribbles/SCRSlider.m
@@ -18,28 +18,37 @@
         
         self.backgroundColor = [UIColor redColor];
         
-//        UIButton * sizeButton = [[UIButton alloc] initWithFrame: CGRectMake(10, 10, 30, 30)];
+        
+        
+//       UIButton * sizeButton = [[UIButton alloc] initWithFrame: CGRectMake(10, 10, 100, 30)];
 //        
-//        sizeButton.backgroundColor = [UIColor blackColor];
-//        sizeButton.layer.cornerRadius = 15;
-//        [self addSubview:sizeButton];
+//       sizeButton.backgroundColor = [UIColor blackColor];
+//       sizeButton.layer.cornerRadius = 15;
+//       [self addSubview:sizeButton];
+        
+    
+        
+        UIView * lineView = [[UIView alloc] initWithFrame:CGRectMake(50, 10, 1, 200)];
+        lineView.backgroundColor = [UIColor blackColor];
+        [self addSubview:lineView];
         
     }
     return self;
 }
 
-
+/*
  Only override drawRect: if you perform custom drawing.
  An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
    // Drawing code
     
-    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
  
 
 }
-
+*/
 
 @end
  
