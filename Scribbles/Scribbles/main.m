5ed5427e909f860870f28c9078f21e75b19714f8
Shane Sniteman <ssniteman@gmail.com>
2014-08-04 09:17:19 -0400

    Initial Commit

---
+++ Scribbles/Scribbles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scribbles
+//
+//  Created by Shane Sniteman on 8/4/14.
+//  Copyright (c) 2014 Shane Sniteman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCRAppDelegate class]));
+    }
+}
